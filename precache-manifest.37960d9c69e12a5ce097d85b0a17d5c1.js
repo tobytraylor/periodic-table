0818290350d7a188e91e76b7c4f77112bca0df0a
unknown <toby.traylor@gmail.com>
2020-04-16 12:57:16 -0600

    Updates

---
+++ precache-manifest.37960d9c69e12a5ce097d85b0a17d5c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "69965db0447d633eda1c7a479db0cac3",
+    "url": "/periodic-table/index.html"
+  },
+  {
+    "revision": "bc83ee49035735f56b5c",
+    "url": "/periodic-table/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "00fdfdf0f4a4e21c4157",
+    "url": "/periodic-table/static/js/2.d5b59487.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/periodic-table/static/js/2.d5b59487.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bc83ee49035735f56b5c",
+    "url": "/periodic-table/static/js/main.d5597b62.chunk.js"
+  },
+  {
+    "revision": "4889c7b0fe2e4daf2d8c",
+    "url": "/periodic-table/static/js/runtime-main.908b57be.js"
+  }
+]);
