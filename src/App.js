fb2a819e9f67f71ae5f5439c0f7dd850e3ea058d
unknown <toby.traylor@gmail.com>
2020-09-07 19:30:29 -0600

    added scores, fit on one screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState, ReactFragment} from 'react';
+import React, {useState, useEffect, ReactFragment} from 'react';
 import './App.css';
 import elements from './elements.json';
 import { DndProvider, useDrag, useDrop } from 'react-dnd'
@@ -6,36 +6,35 @@ import Backend from 'react-dnd-html5-backend'
 
 // TODO: Drag and drop shows a pointer, allows dirty cheaters, dirty dirty cheaters, hate them
 
-// let cookiesPerDayForToday = [1,5,2,4,6,5,4,3,2,1]
-// let familyMember = ['sheryl', 'toby', 'brody', 'cj']
-// let familyMembersToCookies = {'sheryl': 3, 'toby': 2, 'brody': 5001, 'cj': 6}
-// let familyMembersToDailyHistory = {'sheryl': [5,1,2,3,1], 'toby': [52,5,2,1], 'brody': [6,1,2,3,1,1], 'cj': 6}
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
 
+function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(getWindowDimensions());
 
-// class Person {
-//   constructor(name, height, weight) {
-//     this.name = name;
-//     this.height = height;
-//     this.weight = weight;
-//   }
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
 
-// }
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
 
-// let familyToLotsO = {'sheryl': Person('Sheryl Traylor', 110, 12*5+4), 'brody': Person(2,7)}
+  return windowDimensions;
+}
 
-// function square(x) {
-//   return x * x;
-// }
-// let answer = square(5)
-
-// function muliplyNumbers(q, y) {
-//   return x * y;
-// }
-
-// let result = multiplyNumbers(15, 45);
 
 const Slot = (props) => {
-  const { number, state, setState } = props; 
+  const { number, state, setState, incrementMissed } = props; 
+  const { height, width } = useWindowDimensions();
+  const size = `${Math.min(height, width) * .05}px`
+
   if (!state) {
     throw `blah ${number}`
   }
@@ -44,13 +43,19 @@ const Slot = (props) => {
     accept: "TILE",
     //drop: () => moveKnight(x, y),
     canDrop: (item, monitor) => {
-      console.log(JSON.stringify(item))
-      return (item.number == number)
+      return true
+      // console.log(JSON.stringify(item))
+      // return (item.number == number)
     },
     drop: (item, monitor) => {
-      let newState = [...state]
-      newState[number-1] = true
-      setState(newState)
+      if (item.number == number) {
+        let newState = [...state]
+        newState[number-1] = true
+        setState(newState)
+      } else {
+        incrementMissed()
+      }
+      
     },
     collect: (mon) => ({
       isOver: !!mon.isOver()
@@ -63,15 +68,14 @@ const Slot = (props) => {
   if(isFilled) {
     return (
       <div
-          style={{width: '4vw', 
-                  height: '4vw', 
+          style={{width: size, 
+                  height: size, 
                   border: '2px solid black', 
                   margin: "1px", 
                   display: 'inline-block',
                   background: '#9fa1d4',
                   overflow: 'hidden',
                   textAlign: "center"}}>
-        {/* <div style={{fontSize: ".5vw"}}>{number}</div> */}
         <div style={{fontSize: '2vw'}}>{elementData.symbol}</div>
         <div style={{fontSize: '.7vw'}}>{elementData.name}</div>
       </div>
@@ -79,8 +83,8 @@ const Slot = (props) => {
   } else {
     return (
       <div ref={drop}  
-          style={{width: '4vw', 
-                  height: '4vw', 
+          style={{width: size, 
+                  height: size, 
                   border: '2px solid black', 
                   margin: "1px", 
                   color: '#eee',
@@ -92,7 +96,6 @@ const Slot = (props) => {
                   }}>
           <div>
             <div style={{fontSize: ".9vw"}}>{number}</div>
-            {/* <div style={{fontSize: "2vw"}}>{elementData.symbol}</div> */}
           </div>
       </div>
     )
@@ -100,27 +103,59 @@ const Slot = (props) => {
  
 }
 
+const Spacer = () => {
+  const { height, width } = useWindowDimensions();
+  const size = `${Math.min(height, width) * .05}px`;
+  return (
+    <div style={{width: size, 
+                height: size,
+                border: '2px solid white', 
+                margin: "1px", 
+                display: 'inline-block',
+                background: "#fff",
+                overflow: 'hidden',
+            }}>
+    </div>
+  ) 
+}
+
+
+
+const ElementTile = (props) => {
+  const { number, symbol, name, atomicMass } = props; 
+  const { height, width } = useWindowDimensions();
+  const size = `${Math.min(height, width) * .05}px`;
+  const [ {isDragging}, drag] = useDrag({
+    item: { type: 'TILE', number: number },
+		collect: monitor => ({
+			isDragging: !!monitor.isDragging(),
+		}),
+  })
+  return (
+    <div ref={drag} 
+        style={{width: size, 
+                height: size, 
+                border: '2px solid black', 
+                margin: "1px", 
+                display: 'inline-block',
+                background: '#9fa1d4',
+                textAlign: "center",
+                opacity: isDragging ? 0.5 : 1}}>
+
+      <div style={{fontSize: '3vh'}}>{symbol}</div>
+      <div style={{fontSize: '1.2vh'}}>{name}</div>
+    </div>
+  )
+}
+
 const SlotRow = (props) => {
-  const { from, to, state, setState } = props;
+  const { from, to, state, setState, incrementMissed} = props;
   let boxes = [];
   for (let i=from; i <= to; i++) {
-    boxes.push(<Slot key={i} number={i} state={state} setState={setState}/>)
+    boxes.push(<Slot key={i} number={i} state={state} setState={setState} incrementMissed={incrementMissed}/>)
   }
   return boxes;
 }
-
-const Spacer = () => (
-  <div style={{width: '4vw', 
-        height: '4vw',
-        border: '2px solid white', 
-        margin: "1px", 
-        display: 'inline-block',
-        // boxShadow: 'inset 0px 0px 9px #005060',
-        background: "#fff",
-        overflow: 'hidden',
-    }}>
-  </div>
-)
 
 const SpacerRow = (props) => {
   let boxes = [];
@@ -130,68 +165,39 @@ const SpacerRow = (props) => {
   return boxes;
 }
 
-
-// TODO: (Toby Traylor) wrap problem
-
-const ElementTile = (props) => {
-  const { number, symbol, name, atomicMass } = props; 
-  const [ {isDragging}, drag] = useDrag({
-    item: { type: 'TILE', number: number },
-		collect: monitor => ({
-			isDragging: !!monitor.isDragging(),
-		}),
-  })
+const PeriodicTableSlots = ({state, setState, incrementMissed}) => {
   return (
-    <div ref={drag} 
-        style={{width: '4vw', 
-        height: '4vw', 
-        border: '2px solid black', 
-        margin: "1px", 
-        display: 'inline-block',
-        background: '#9fa1d4',
-        textAlign: "center",
-        opacity: isDragging ? 0.5 : 1}}>
-
-      <div style={{fontSize: '2vw'}}>{symbol}</div>
-      <div style={{fontSize: '.7vw'}}>{name}</div>
-    </div>
-  )
-}
-
-const PeriodicTableSlots = ({state, setState}) => {
-  return (
-    // style={{backgroundImage: 'linear-gradient(217deg, #DDA0DD, #50EFFF)', height: '100vh'}
     <div style={{textAlign: 'center'}}>
       <div>
-        <Slot number={1} state={state} setState={setState}/>
+        <Slot number={1} state={state} setState={setState} incrementMissed={incrementMissed}/>
         <SpacerRow count={16} state={state} setState={setState}/>
-        <Slot number={2} state={state} setState={setState}/>
+        <Slot number={2} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
       <div>
-        <SlotRow from={3} to={4} state={state} setState={setState}/>
+        <SlotRow from={3} to={4} state={state} setState={setState} incrementMissed={incrementMissed}/>
         <SpacerRow count={10} state={state} setState={setState}/>
-        <SlotRow from={5} to={10} state={state} setState={setState}/>
+        <SlotRow from={5} to={10} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
       <div>
-        <SlotRow from={11} to={12} state={state} setState={setState}/>
+        <SlotRow from={11} to={12} state={state} setState={setState} incrementMissed={incrementMissed}/>
         <SpacerRow count={10} state={state} setState={setState}/>
-        <SlotRow from={13} to={18} state={state} setState={setState}/>
+        <SlotRow from={13} to={18} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
       <div>
-        <SlotRow from={19} to={36} state={state} setState={setState}/>
+        <SlotRow from={19} to={36} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
       <div>
-        <SlotRow from={37} to={54} state={state} setState={setState}/>
+        <SlotRow from={37} to={54} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
       <div>
-        <SlotRow from={55} to={56} state={state} setState={setState}/>
+        <SlotRow from={55} to={56} state={state} setState={setState} incrementMissed={incrementMissed}/>
         <Spacer />
-        <SlotRow from={72} to={86} state={state} setState={setState}/>
+        <SlotRow from={72} to={86} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
       <div>
-        <SlotRow from={87} to={88} state={state} setState={setState}/>
+        <SlotRow from={87} to={88} state={state} setState={setState} incrementMissed={incrementMissed}/>
         <Spacer />
-        <SlotRow from={104} to={118} state={state} setState={setState}/>
+        <SlotRow from={104} to={118} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
       <div>
         <Spacer />
@@ -200,14 +206,14 @@ const PeriodicTableSlots = ({state, setState}) => {
       <Spacer />
         <Spacer />
         <Spacer />
-        <SlotRow from={57} to={71} state={state} setState={setState}/>
+        <SlotRow from={57} to={71} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
      
       <div>
         <Spacer />
         <Spacer />
         <Spacer />
-        <SlotRow from={89} to={103} state={state} setState={setState}/>
+        <SlotRow from={89} to={103} state={state} setState={setState} incrementMissed={incrementMissed}/>
       </div>
     </div>
   );
@@ -217,6 +223,15 @@ const PeriodicTableSlots = ({state, setState}) => {
 
 const App = () => {
   const [table, setTable] = useState(elements.map((x) => {return false}))
+  const [missed, setMissed] = useState(0)
+  const correct = table.filter(x => x == true).length
+  
+  const incrementMissed = () => setMissed(missed+1) 
+  const reset = () => {
+    setTable(elements.map((x) => {return false}))
+    setMissed(0)
+  }
+  
   let options = []
   for (var i=0; i< table.length; i++) {
     if (!table[i]) {
@@ -228,15 +243,27 @@ const App = () => {
   return (
     <DndProvider backend={Backend}>
       <React.Fragment>
-        <PeriodicTableSlots state={table} setState={setTable}></PeriodicTableSlots>
-
-        {options.map((element) => 
-          <ElementTile  key={element.atomicNumber}
+        <table>
+          <tr style={{background: 'green', color: 'white'}}>
+            <td>Correct</td>
+            <td>{correct}</td>
+          </tr>
+          <tr style={{background: 'red', color: 'white'}}>
+            <td >Missed</td>
+            <td>{missed}</td>
+          </tr>
+          
+        </table>
+        <button onClick={reset}>Reset</button>
+        <PeriodicTableSlots state={table} setState={setTable} incrementMissed={incrementMissed}></PeriodicTableSlots>
+        <div>
+          {options.map((element) => 
+            <ElementTile  key={element.atomicNumber}
                         number={element.atomicNumber} 
                         symbol={element.symbol} 
                         name={element.name} 
                         atomicMass={element.atomicMass} ></ElementTile> )}
-        
+        </div>
       </React.Fragment>
     </DndProvider>
   )
